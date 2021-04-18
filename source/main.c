5c36aba8dff4752d2f7865532ba40c8540370cc6
Rishab Dudhia <rdudh001@wch136-24.cs.ucr.edu>
2021-04-17 17:48:11 -0700

    Working on exercise 1

--- source/main.c
+++ source/main.c
@@ -1,9 +1,9 @@
-/*	Author: rdudh001
+/*	Author: Rishab Dudhia (rdudh001)
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section:022
+ *	Assignment: Lab #4  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
- *
+ *	Initially PB0 on and pressing PA0 turns B0 off and B1 on; repeat
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
@@ -12,12 +12,76 @@
 #include "simAVRHeader.h"
 #endif
 
+enum States { smstart, pb0_on, pb0_wait, pb1_on, pb1_wait } state;
+
+unsigned char Tick(unsigned char cntA0)
+{
+    switch(state)
+    {
+	case smstart:
+	    state = pb0_on;
+	    break;
+	case pb0_on:
+	    state = pb0_wait;
+	    break;
+	case pb0_wait:
+	    if((PINA & 0x01) == 0x01)
+	    {
+		state = pb1_on;
+	    }
+	    else
+	    {
+		state = pb0_wait;
+	    }
+	    break;
+	case pb1_on:
+	    state = pb1_wait;
+	    break;
+	case pb1_wait:
+	    if ((PINA & 0x01) == 0x01)
+	    {
+		state = pb0_on;
+	    }
+	    else
+	    {
+		state = pb1_wait;
+	    }
+	    break;
+
+	    default:
+		state = smstart;
+		break;
+    }
+
+    switch(state)
+    {
+        case smstart:
+        case pb0_on:
+            PORTB = 0x01;
+	    return cntA0;
+        case pb0_wait:
+            PORTB = 0x01;
+            return (cntA0 + 1);
+        case pb1_on:
+            PORTB = 0x02;
+	    return cntA0;
+        case pb1_wait:
+            PORTB = 0x02;
+            return (cntA0 + 1);
+
+        default:
+            return cntA0;
+    }
+}
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    unsigned char cntA0 = 0x00;
     /* Insert your solution below */
     while (1) {
-
+	state = smstart;
+	cntA0 = Tick(cntA0);
     }
-    return 1;
+    return 0;
 }
