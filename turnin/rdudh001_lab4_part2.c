460f804b9943470399074aeb071f4a906858614e
rishabdudhia <81605069+rishabdudhia@users.noreply.github.com>
2021-04-25 21:13:53 -0700

    Updated part2

--- turnin/rdudh001_lab4_part2.c
+++ turnin/rdudh001_lab4_part2.c
@@ -1,18 +1,20 @@
 /*	Author: Rishab Dudhia (rdudh001)
  *  Partner(s) Name: 
- *	Lab Section:022
- *	Assignment: Lab #4  Exercise #2
+ *	Lab Section:
+ *	Assignment: Lab #5  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
- *	A0 increments; A1 decrements; PORTC starts at 7
+ *	Lab 4 exercise 2 on microcontroller
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
  */
+
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-enum States {smstart, wait, inc, dec, inc_wait, dec_wait, reset } state;
+enum States {smstart, wait, inc, dec, inc_wait, dec_wait, reset_wait, reset } state;
 unsigned char cntA0;
 unsigned char cntA1;
 void Tick()
@@ -25,33 +27,40 @@ void Tick()
 	    state = wait;
 	    break;
 	case wait:
-	    if (((PINA & 0x03) == 0x01) && (PORTC < 0x09))
+	    if (((~PINA & 0x03) == 0x01) && (PORTC < 0x09))
 	    {
 		state = inc;
 	    	cntA0 = cntA0 + 1;
 	    }
-	    else if (((PINA & 0x03) == 0x02) && (PORTC > 0x00))
+	    else if (((~PINA & 0x03) == 0x02) && (PORTC > 0x00))
 	    {
 		state = dec;
 	        cntA1 = cntA1 + 1;
 	    }
+	    /*else if ((PINA & 0x03) == 0x03)
+	    {
+		    state = reset;
+	    	    cntA0 = cntA0 + 1;
+		    cntA1 = cntA1 + 1;
+	    }*/
 	    else
 	    {
 		state = wait;
 	    }
 	    break;
 	case inc_wait:
-	    if((PINA & 0x03) == 0x01)
+	    if((~PINA & 0x03) == 0x01)
 	    {
 		state = inc_wait;
 	    }
-	    else if ((PINA & 0x03) == 0x02)
+	    else if ((~PINA & 0x03) == 0x02)
 	    {
 		state = dec;
 		cntA1 = cntA1 + 1;
 	    }
-	    else if ((PINA & 0x03) == 0x00)
+	    else if ((~PINA & 0x03) == 0x03)
 	    {
+		cntA1 = cntA1 + 1;
 		state = reset;
 	    }
 	    else
@@ -60,23 +69,30 @@ void Tick()
 	    }
 	    break;
 	case dec_wait:
-	    if ((PINA & 0x03) == 0x02)
+	    if ((~PINA & 0x03) == 0x02)
 	    {
 		state = dec_wait;
 	    }
-	    else if ((PINA & 0x03) == 0x01)
+	    else if ((~PINA & 0x03) == 0x01)
 	    {
 		state = inc;
 		cntA0 = cntA0 + 1;
 	    }
-	    else if ((PINA & 0x03) == 0x00)
+	    else if ((~PINA & 0x03) == 0x03)
 	    {
+		cntA0 = cntA0 + 1;
 		state = reset;
 	    }
 	    else
 	    {
 		state = wait;
 	    }
+	    break;
+	case reset_wait:
+	    if ((~PINA & 0x03) == 0x00)
+		    state = wait;
+	    else
+		    state = reset_wait;
 	    break;
 	case inc:
 	    state = inc_wait;
@@ -98,6 +114,7 @@ void Tick()
         case smstart:
 	    PORTC = 0x07;
         case wait:
+	case reset_wait:
         case inc_wait:
         case dec_wait:
 	    break;
